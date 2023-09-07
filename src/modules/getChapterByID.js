9cfc42f8debe897b3c86081b2820292b01b2ba2f
TzurS11 <90769470+TzurS11@users.noreply.github.com>
2023-09-07 03:37:17 +0300

    switch from puppeteer to axios

--- src/modules/getChapterByID.js
+++ src/modules/getChapterByID.js
@@ -1,56 +1,28 @@
-const puppeteer = require("puppeteer");
+const { fetchHTML } = require("./utils");
+
 /**
  * Get all images from a chapter by. get chapter id from getByID
  * @param {string} chapterID
  */
 async function getChapterByID(chapterID) {
-  const browser = await puppeteer.launch({ headless: "new" });
-  const page = await browser.newPage();
-
-  await page.setRequestInterception(true);
-
   try {
-    page.on("request", (request) => {
-      if (
-        ["image", "stylesheet", "fetch", "font", "media"].includes(
-          request.resourceType()
-        )
-      ) {
-        request.abort();
-      } else {
-        request.continue();
+    const document = await fetchHTML(
+      `https://bato.to/title/${chapterID}?load=2`
+    );
+    const astroisland = document.getElementsByTagName("astro-island");
+    const pages = [];
+    for (let i = 0; i < astroisland.length; i++) {
+      const propsJSON = JSON.parse(astroisland.item(i).getAttribute("props"));
+      if (propsJSON.imageFiles) {
+        const imagesArray = JSON.parse(propsJSON.imageFiles[1]);
+        for (let j = 0; j < imagesArray.length; j++) {
+          pages.push(imagesArray[j][1]);
+        }
       }
-    });
-
-    await page.goto(`https://bato.to/title/${chapterID}?load=2`, {
-      waitUntil: "domcontentloaded",
-    });
-
-    let imagesSelector =
-      ".z-10.absolute.top-0.right-0.bottom-0.left-0.w-full.h-full";
-
-    await page.waitForSelector(imagesSelector);
-
-    let pagesArray = [];
-
-    const elementsWithClassName = await page.$$(imagesSelector);
-
-    for (const element of elementsWithClassName) {
-      const src = await element.evaluate((node) => node.getAttribute("src"));
-      pagesArray.push(src);
     }
-
-    await browser.close();
-    return {
-      pages: pagesArray,
-      successful: true,
-    };
+    return { valid: false, pages: pages };
   } catch (error) {
-    await browser.close();
-    return {
-      pages: [],
-      successful: false,
-    };
+    return { valid: false, pages: [] };
   }
 }
 

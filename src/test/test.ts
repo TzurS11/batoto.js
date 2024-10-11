8b2395741d5eb2b277491990328d61a2026b7997
TzurS11 <90769470+TzurS11@users.noreply.github.com>
2024-10-11 14:39:23 +0300

    Update test.ts

--- src/test/test.ts
+++ src/test/test.ts
@@ -1,16 +1,36 @@
 // import axios from "axios";
-import { getByID, getRandom } from "..";
+import { getByID, getChapterByID, searchByKeyword } from "..";
 
-(async () => {
-  let manga = await getByID("81514-solo-leveling-official");
-  if (!manga.valid) throw new Error("by id");
-  const pages = await manga.results.chapters[0].getChapter();
-  console.log(pages.pages);
-  let random = await getRandom();
-  if (!random.valid) throw new Error("by random");
-  const randomDetailed = await random.results[0].getAdditionalInfo();
-  if (!randomDetailed.valid) throw new Error("by random detailed");
-  const randomChapter = await randomDetailed.results.chapters[0].getChapter();
-  if (!randomChapter.valid) throw new Error("by random chapter");
-  console.log(randomChapter.pages);
-})();
+async function getFirstChapter(title: string) {
+  //searching by manga name
+  let search = await searchByKeyword(title);
+
+  //check if there are results.
+  if (!search.valid) {
+    // No results found.
+    return [];
+  }
+
+  //finished searching. top result is index 0
+  let topResult = search.results[0];
+
+  // get information about the search by its id. this includes chapters
+  let topManga = await getByID(topResult.id);
+  if (!topManga.valid) {
+    // "Error getting the manga with the specified id."
+    return [];
+  }
+  // got manga and all chapters
+
+  // get list of images from the chapter. this might take a second to load
+  let chapter = await getChapterByID(topManga.results.chapters[0].id);
+  if (!chapter.valid) {
+    // Error getting the chapters with the specified chapter ID.
+    return [];
+  }
+  console.log(chapter.pages);
+  return chapter.pages;
+}
+// returns empty array if no results were found or there is an error.
+// returns string arrray if there are pages for a chapter.
+getFirstChapter("Demon Slayer");
